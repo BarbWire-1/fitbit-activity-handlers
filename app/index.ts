e5e5df148fbf2a77f4d88e034ad6d663af73fbbe
Barb <clockery69@gmail.com>
2021-04-10 13:14:33 +0200

    converted playground to .ts

--- app/index.ts
+++ app/index.ts
@@ -1,17 +1,17 @@
-import clock from 'clock'
+import clock from 'clock';
 import { standardHandler, azmHandler, hourlyStepsHandler } from './handlers'
 
-const handlers
+let handlers;
 
 //********************************************************************************************* Start-up *****
 
 ;(function() {       //initialisation IIFE
-  const myEnergyHandler = standardHandler('calories')
-  const myStepsHandler = standardHandler('steps')
-  const myDistHandler = standardHandler('distance')
-  const myFloorsHandler = standardHandler('elevationGain')
-  const myAzmHandler = azmHandler()
-  const myHourlyStepsHandler = hourlyStepsHandler()
+  const myEnergyHandler = standardHandler('calories');
+  const myStepsHandler = standardHandler('steps');
+  const myDistHandler = standardHandler('distance');
+  const myFloorsHandler = standardHandler('elevationGain');
+  const myAzmHandler = azmHandler();
+  const myHourlyStepsHandler = hourlyStepsHandler();
   handlers = [myEnergyHandler, myStepsHandler, myDistHandler, myFloorsHandler, myAzmHandler, myHourlyStepsHandler]
 
   clock.granularity = 'seconds'
